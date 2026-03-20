dfdcfe24f0f7c36035fdd0f7656fd0d6b16a7e99
Ninja@07 <siludas978@gmail.com>
2026-03-21 03:27:55 +0530

    small changes

---
+++ 08_decorators/01_solution.py
@@ -0,0 +1,12 @@
+import time
+
+def timer(func) :
+    def wrapper(*args, **kwargs) :
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} ran in {end - start} time")
+        
+        return result
+    return wrapper
+
